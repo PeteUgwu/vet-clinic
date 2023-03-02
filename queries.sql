eedce60456560f90076638c28593fea6bdd07b10
peter <peterugwu64@gmail.com>
2023-03-02 16:54:54 +0100

    Review and update queries.sql file

--- queries.sql
+++ queries.sql
@@ -1,10 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * From animals WHERE name LIKE '%mon%';
-SELECT * From animals WHERE date_of_birth BETWEEN DATE '2016-01-01' AND DATE '2019-12-31';
-SELECT * From animals WHERE neutered = 'YES' AND escape_attempts < 3;
-SELECT date_of_birth from animals WHERE name IN ('Agumon','Pikachu');
-SELECT name, date_of_birth FROM animals WHERE weight_kg > 10.5;
-SELECT * From animals WHERE neutered = 'YES';
-SELECT * From animals WHERE name <> 'Gabumon';
-SELECT * From animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+SELECT * FROM animals WHERE name LIKE '%mon%';
+SELECT name FROM animals WHERE date_of_birth BETWEEN DATE '2016-01-01' AND DATE '2019-12-31';
+SELECT name FROM animals WHERE neutered = 'YES' AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon','Pikachu');
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = 'YES';
+SELECT * FROM animals WHERE name <> 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
